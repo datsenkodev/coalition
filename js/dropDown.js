fe692ddad828f7db844eac009421b1e7b994a4b7
Dmytro <datsenkodev@gmail.com>
2025-02-03 18:21:34 +0200

    Add header and stats section

--- js/dropDown.js
+++ js/dropDown.js
@@ -1,42 +1,39 @@
-document.querySelectorAll('.dropdown-only').forEach((button) => {
-  button.addEventListener('click', function (event) {
-    event.stopPropagation();
+document.addEventListener('DOMContentLoaded', () => {
+  const dropdownButtons = document.querySelectorAll('.dropdown-only');
 
-    const isActive = this.classList.contains('active');
+  dropdownButtons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      event.preventDefault();
 
-    closeAllDropdowns();
-    if (!isActive) {
-      this.classList.add('active');
+      const isActive = this.classList.contains('active');
+
+      closeAllDropdowns();
+      if (!isActive) {
+        this.classList.add('active');
+      }
+    });
+  });
+
+  document.addEventListener('click', function (event) {
+    if (!event.target.closest('.dropdown-only')) {
+      closeAllDropdowns();
     }
   });
-});
 
-function closeAllDropdowns() {
-  document.querySelectorAll('.dropdown-only').forEach((button) => {
-    button.classList.remove('active');
-  });
-}
-
-document.addEventListener('click', function (event) {
-  if (!event.target.closest('.dropdown-only') && !event.target.closest('.burger-btn')) {
-    closeAllDropdowns();
+  function closeAllDropdowns() {
+    dropdownButtons.forEach((button) => {
+      button.classList.remove('active');
+    });
   }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.querySelector('.burger');
-  const burgerMenuContent = document.querySelector('.burger-menu-content');
-  const dropdowns = document.querySelectorAll('.dropdown-only');
+// document.addEventListener('DOMContentLoaded', () => {
+// const burger = document.querySelector('.burger');
+// const burgerMenuContent = document.querySelector('.burger-menu-content');
 
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    burgerMenuContent.classList.toggle('active');
-    document.body.classList.toggle('no-scroll');
-  });
-
-  dropdowns.forEach((dropdown) => {
-    dropdown.addEventListener('click', () => {
-      dropdown.classList.toggle('active');
-    });
-  });
-});
+// burger.addEventListener('click', () => {
+//   burger.classList.toggle('active');
+//   burgerMenuContent.classList.toggle('active');
+//   document.body.classList.toggle('no-scroll');
+// });
+// });
